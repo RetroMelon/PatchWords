2d2be130cb5b38211288404d4d86ae8152943b5b
2078421 <harry-moore@live.co.uk>
2015-03-03 16:18:01 +0000

    models...

--- patchwords_project/patchwords/models.py
+++ patchwords_project/patchwords/models.py
@@ -1,10 +1,22 @@
+from datetime import date
 from django.db import models
 from django.contrib.auth.models import User
-from django.template.defaultfilters import slugify
+#from django.template.defaultfilters import slugify
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    picture = models.ImageField(upload_to='profile_images',blank=true)
+    picture = models.ImageField(upload_to='profile_images',blank=True)
+    bio = models.TextField(blank=True)
+    date_of_birth = models.DateField(blank=True)
+    GENDER_CHOICES = (
+        ('Male', 'Male'), ('Female', 'Female'),
+    )
+    gender = models.CharField(choices=GENDER_CHOICES)
+
+    def calculate_age(self):
+        today = date.today()
+        date_of_birth = self.date_of_birth
+        return today.year - date_of_birth.year - ((today.month, today.day) < (date_of_birth.month, date_of_birth.day))
 
     def __unicode__(self):
         return self.user.username
@@ -20,36 +32,36 @@ class Category(models.Model):
 
 class Paragraph(models.Model):
     story = models.ForeignKey(Story)
-    parent = ForeignKey(Paragraph)
+    parent = models.ForeignKey('self')
     views = models.IntegerField(default=0)
     author = models.ForeignKey(User)
     created_datetime = models.DateTimeField(auto_now_add = True)
-    end = BooleanField(default=false)
+    end = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.author.username
 
 
 class Favourite(models.Model):
-    user = ForeignKey(user.username)
+    user = models.ForeignKey(UserProfile)
     story = models.ForeignKey(Story)
 
     def __unicode__(self):
             return self.user.username
 
 
-class Story(models.Models):
+class Story(models.Model):
     title = models.CharField(max_length = 128, unique=True)
     author = models.ForeignKey(User)
     category = models.ForeignKey(Category)
-    dateTime = models.DateTimeFeidl(auto_now_add = True)
-    slug = model.SlugField()
+    dateTime = models.DateTimeField(auto_now_add = True)
+    slug = models.SlugField()
 
     def __unicode__(self):
         return self.title
 
 
-class Like(models.Models):
+class Like(models.Model):
     user = models.ForeignKey(User)
     paragraph = models.ForeignKey(Paragraph)
 
