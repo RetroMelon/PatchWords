5e39e635677909a5a245f61d60a0ed429e8ccf15
2087186m <2087186m@student.gla.ac.uk>
2015-03-17 15:39:13 +0000

    Started Search

--- patchwords_project/patchwords/urls.py
+++ patchwords_project/patchwords/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     url(r'^$', home, name='home'),
     url(r'^story/(?P<story_name_slug>[/w/-]+)/$', story, name='story'),
     url(r'^category/(?P<category_name_slug>[/w/-]+)/$', category, name='category'),
-
+    url(r'^search/q', search, name='search'),
     #allows the homepage/categories page to request more of the most popular stories.
     url(r'^gettopstories', get_top_stories, name='get_top_stories'),
 )
